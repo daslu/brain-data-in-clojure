ade3aa509319e9671c8a02b3e67ba2a88e350b2b
daslu <daniel.slutsky@gmail.com>
2025-04-28 23:51:23 +0300

    added type hints and dtype-next

--- src/bci_project/brain.clj
+++ src/bci_project/brain.clj
@@ -1,7 +1,12 @@
 (ns bci-project.brain
-  (:require [clojure.pprint :refer [pprint]])
-  (:import [us.hebi.matlab.mat.format Mat5]
+  (:require [bci-project.signal :as sig]
+            [clojure.pprint :refer [pprint]]
+            [tech.v3.tensor :as tensor])
+  (:import [us.hebi.matlab.mat.format MatStruct Mat5 Mat5File]
            [us.hebi.matlab.mat.types Struct Array Matrix Cell]))
+
+(set! *warn-on-reflection* true)
+
 
 (def eeg-data-atom (atom nil))
 
@@ -12,7 +17,7 @@
   {:real real :imag imag})
 
 (defn convert-element
-  "Convert a MATLAB element to appropriate Clojure data structure"
+  "MATLAB element to appropriate Clojure data structure"
   [element]
   (cond
     (instance? Struct element) (struct->map element)
@@ -21,20 +26,20 @@
     :else nil))
 
 (defn cell->vec
-  "Convert a MATLAB cell array to a Clojure vector"
-  [cell]
+  "MATLAB cell array to a Clojure vector"
+  [^Cell cell]
   (when cell
     (let [dims (.getDimensions cell)
-          rows (first dims) 
+          rows (first dims)
           cols (second dims)]
-      (vec (for [i (range rows)]
-             (vec (for [j (range cols)]
+      (vec (for [^int i (range rows)]
+             (vec (for [^int j (range cols)]
                     (let [element (.get cell i j)]
                       (convert-element element)))))))))
 
 (defn array->vec
-  "Convert a MATLAB array to a Clojure vector"
-  [array]
+  "MATLAB array to a Clojure vector"
+  [^Array array]
   (when array
     (let [dims (.getDimensions array)
           rows (first dims)
@@ -44,17 +49,18 @@
           (.isLogical array) (.getBoolean array 0)
           (.isComplex array) (complex (.getDouble array 0) (.getImaginaryDouble array 0))
           :else (.getDouble array 0))
-        (vec (for [i (range rows)]
-               (vec (for [j (range cols)]
+        (vec (for [^int i (range rows)]
+               (vec (for [^int j (range cols)]
                       (try
                         (if (.isComplex array)
+                          
                           (complex (.getDouble array i j) (.getImaginaryDouble array i j))
                           (.getDouble array i j))
                         (catch Exception _ nil))))))))))
 
 (defn matrix->vec
-  "Convert a verbose MATLAB matrix to a Clojure vector up to 2-dimensions."
-  [matrix]
+  "MATLAB matrix to a Clojure vector up to 2-dimensions."
+  [^Matrix matrix]
   (when matrix
     (let [dims (.getDimensions matrix)
           total-elements (apply * dims)]
@@ -67,20 +73,32 @@
                         :type (str (.getType matrix))
                         :elements total-elements}
               values (case (count dims)
-                       1 (vec (for [i (range (first dims))]
-                                (.getDouble matrix i)))
-                       2 (vec (for [i (range (first dims))]
-                                (vec (for [j (range (second dims))]
-                                       (.getDouble matrix i j)))))
+                       1
+                       ;; (vec (for [^int i (range (first dims))]
+                       ;;        (.getDouble matrix i)))
+                       (tensor/compute-tensor dims
+                                              (fn [^long i]
+                                                (.getDouble matrix i))
+                                              :float32)
+                       2
+                       ;; (vec (for [^int i (range (first dims))]
+                       ;;        (vec (for [^int j (range (second dims))]
+                       ;;               (.getDouble matrix i j)))))
+                       (tensor/compute-tensor dims
+                                              (fn [^long i
+                                                   ^long j]
+                                                (.getDouble matrix i j))
+                                              :float32)
                        (throw (Exception. (str "Unsupported dimension count: " (count dims)))))]
           (assoc metadata :values values))))))
 
+(set! *warn-on-reflection* true)
 (defn struct->map
-  "Verbose MATLAB struct to a Clojure map"
-  [struct]
+  "MATLAB struct to a Clojure map"
+  [^Struct struct]
   (when struct
-    (let [field-names (.getFieldNames struct)]
-      (reduce (fn [acc field-name]
+    (let [field-names (.getFieldNames struct)]; it takes effort for the JVM to know what to do
+      (reduce (fn [acc field-name]            ; this is called reflection
                 (let [value (.get struct field-name)]
                   (assoc acc (keyword field-name)
                          (convert-element value))))
@@ -90,8 +108,8 @@
 (defn read-eeg-data
   "Read EEG data from a MATLAB file and convert to a Clojure map"
   [file-path]
-  (let [mat-file (Mat5/readFromFile file-path)
-        eeg-struct (.getStruct mat-file "eeg")]
+  (let [mat-file ^Mat5File (Mat5/readFromFile ^String file-path)
+        eeg-struct ^MatStruct (.getStruct mat-file "eeg")]
     (struct->map eeg-struct)))
 
 (defn mat-to-edn
@@ -106,27 +124,26 @@
 (defn load-eeg-data!
   "Load EEG data from a MATLAB file into the eeg-data-atom with preprocessing"
   [file-path]
-  (let [mat-file (Mat5/readFromFile (clojure.java.io/file file-path))
+  (reset! eeg-data-atom nil)
+  (let [mat-file (time (Mat5/readFromFile (clojure.java.io/file file-path)))
         eeg-struct (.getStruct mat-file "eeg")
         sample-number (if-let [matches (re-find #"s(\d+)\.mat" file-path)]
                         (second matches)
                         "unknown")
-        data (struct->map eeg-struct)
+        data (time (struct->map eeg-struct))
         processed-data {:sample_number  sample-number
-                        :movement_left  (update (:movement_left data)  :values vec)
-                        :movement_right (update (:movement_right data) :values vec)
-                        :imagery_left   (update (:imagery_left data)   :values vec)
-                        :imagery_right  (update (:imagery_right data)  :values vec)
-                        :movement_event (update (:movement_event data) :values vec)
-                        :imagery_event  (update (:imagery_event data)  :values vec)
-                        :senloc         (update (:senloc data)         :values vec)
-                        :psenloc        (update (:psenloc data)        :values vec)}]
+                        :movement_left   (:movement_left  data) 
+                        :movement_right  (:movement_right data) 
+                        :imagery_left    (:imagery_left   data) 
+                        :imagery_right   (:imagery_right  data) 
+                        :movement_event  (:movement_event data) 
+                        :imagery_event   (:imagery_event  data) 
+                        :senloc          (:senloc         data) 
+                        :psenloc         (:psenloc        data) }]
     (reset! eeg-data-atom processed-data)
     {:data-keys (keys processed-data)
-     :num-dimensions (into {} (map (fn [[k v]] [k (:dims v)])
-                                   (select-keys processed-data [:movement_left  :movement_right
-                                                                :imagery_left   :imagery_right
-                                                                :movement_event :imagery_event
-                                                                :senloc         :psenloc])))}))
-
-#_(load-eeg-data! "resources/data/s02.mat") ;read the relevant parts of a file into the eeg-data-atom
+     :num-dimensions (time (into {} (map (fn [[k v]] [k (:dims v)])
+                                         (select-keys processed-data [:movement_left  :movement_right
+                                                                      :imagery_left   :imagery_right
+                                                                      :movement_event :imagery_event
+                                                                      :senloc         :psenloc]))))}))
